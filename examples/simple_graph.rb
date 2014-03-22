911e20d0248432c1bcb45d3d63a2071dffd542aa
Gabriel González Mañaná <Gabriel@Estilopanda.com>
2014-03-22 11:32:05 -0400

    Renamed test.rb to simple_graph.rb and moved it to examples folder.

--- examples/simple_graph.rb
+++ examples/simple_graph.rb
@@ -1,30 +1,42 @@
 # Console test.
-# 
+#
 # Run this from your console for a simple test,
 # Includes classes, creates a graph, adds four nodes, connects them, deletes one node and prints the circuit.
 # This is a graphical representation of the created graph.
 #
-#        ----2-----
-#       |         |
-#  ---1--         4----
+#         ----2-----
+#        |         |
+#  ---1--          -4-------...
 #       |         |
 #       -----3-----
 #
-# Luego pasa a eliminar el cuarto componenete e imprime, para finalizar
-# Imprimiendo las conecciones (aristas) contenidas en cada uno de los nodos.
 
-load 'edge.rb'
-load 'graph.rb'
-load 'node.rb'
+# Loading all dependencies.
+Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each { |f| load(f) }
 
-puts "Creating graph:"
-puts "-------- ------"
+# Prints specified amount of linebreaks.
+def print_spacing spaces
+  spaces.times do
+    puts "\n"
+  end
+end
+
+# Prints specified message as a heading.
+def print_heading_message message
+  puts message
+
+  message.size.times do
+    print "-"
+  end
+  print_spacing(1)
+end
+
+print_heading_message "Creating graph:"
 a = Graph.new
 puts a.inspect
-puts ""
-puts ""
-puts "Adding components:"
-puts "------ ------------"
+print_spacing(2)
+
+print_heading_message "Adding components:"
 puts a.inspect
 a.add(Node.new(:component_1))
 puts a.node(:component_1).inspect
@@ -34,28 +46,24 @@ a.add(Node.new(:component_3))
 puts a.node(:component_3).inspect
 a.add(Node.new(:component_4))
 puts a.node(:component_4).inspect
-puts ""
+print_spacing(1)
 puts "Graph:"
 puts a.inspect
-puts ""
-puts "Connecting components:"
-puts "---------- -----------"
+print_spacing(1)
 
+print_heading_message "Connecting components:"
 a.node(:component_1).connect_to(a.node(:component_2),"Serie")
 a.node(:component_1).connect_to(a.node(:component_3),"Serie")
 a.node(:component_2).connect_to(a.node(:component_3),"Paralelo")
 a.node(:component_3).connect_to(a.node(:component_4),"Serie")
 a.node(:component_2).connect_to(a.node(:component_4),"Serie")
-
 puts "Edges created!"
-puts ""
+print_spacing(1)
 puts "Graph with the edges:"
 puts a.inspect
-puts ""
-puts ""
-puts "Printing relations (edges):"
-puts "-------- --------- --------"
+print_spacing(2)
 
+print_heading_message "Printing relations (edges):"
 puts "Aristas de 'component_1':"
 puts a.node(:component_1).instance_variable_get(:@out_edges).inspect
 puts "Aristas de 'component_2':"
@@ -64,21 +72,16 @@ puts "Aristas de 'component_3':"
 puts a.node(:component_3).instance_variable_get(:@out_edges).inspect
 puts "Aristas de 'component_4':"
 puts a.node(:component_4).instance_variable_get(:@in_edges).inspect
-puts ""
-puts ""
+print_spacing(2)
 
-puts "Deleting component #4:"
-puts "-------- --------- ---"
 
+print_heading_message "Deleting component #4:"
 a.delete(a.node(:component_4))
-
 puts "Removing!"
-puts ""
-puts ""
-puts "Console Serialization:"
-puts "------- --------------"
+print_spacing(2)
+
+print_heading_message "Console Serialization:"
 a.console_serialization
-puts ""
-puts ""
+print_spacing(2)
 puts "Graph:"
 puts a.inspect
